2d4913585b879af30197d4f5cc486c23619bf5f6
Aryo Skuyyyy <aryoprayoga36yahoo@gmail.com>
2024-09-29 12:06:27 +0700

    baruuuu

--- 07.JavaScript/Praktikkum/Async.js
+++ 07.JavaScript/Praktikkum/Async.js
@@ -1,19 +1,34 @@
-async function fetchData() {
-  try {
+// async function fetchData() {
+//   try {
+//     console.log("Fetching data...");
+
+//     await new Promise((resolve) => {
+//       setTimeout(() => {
+//         resolve();
+//       }, 2000);
+//     });
+
+//     console.log("Data berhasil ditangkap!");
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//   }
+// }
+
+// // Panggil fungsi fetchData
+// fetchData();
+
+function promise() {
+  return new Promise((resolve) => {
     console.log("Fetching data...");
-
-    // Simulasi penundaan dengan setTimeout
-    await new Promise((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, 2000); // Penundaan 2 detik
-    });
-
-    console.log("Data berhasil ditangkap!");
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
+    setTimeout(() => {
+      resolve("Berhasil!");
+    }, 2000);
+  });
 }
 
-// Panggil fungsi fetchData
-fetchData();
+async function async() {
+  const result = await promise();
+  console.log(result);
+}
+
+async();
--- 07.JavaScript/Praktikkum/Clasess.js
+++ 07.JavaScript/Praktikkum/Clasess.js
@@ -1,16 +1,15 @@
 class Person {
-  constructor(name, age) {
-    this.name = name; // Menyimpan nama ke properti name
-    this.age = age; // Menyimpan usia ke properti age
+  constructor(nama, umur) {
+    this.nama = nama;
+    this.umur = umur;
   }
 
   introduce() {
     console.log(
-      `Hello, my name is ${this.name} and I am ${this.age} years old.`
+      `Hello, Nama saya Adalah ${this.nama} dan saya berumur ${this.umur} Tahun.`
     );
   }
 }
 
-// Contoh penggunaan
 const person1 = new Person("Bewok", 22);
-person1.introduce(); // Output: Hello, my name is Alice and I am 30 years old.
+person1.introduce(); //

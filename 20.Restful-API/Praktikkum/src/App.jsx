aea3e7ff3a373797fd74d5a41412f60951b62035
Aryo Skuyyyy <aryoprayoga36yahoo@gmail.com>
2024-10-31 14:56:44 +0700

    penambahan ss REST API

--- 20.Restful-API/Praktikkum/src/App.jsx
+++ 20.Restful-API/Praktikkum/src/App.jsx
@@ -2,11 +2,14 @@ import "./App.css";
 import CreateProduct from "./CreateProduct";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductDetail from "./ProductDetail";
+import Login from "./Login";
+
 function App() {
   return (
     <>
       <Router>
         <Routes>
+          <Route path="/login" element={<Login />} />
           <Route path="/" element={<CreateProduct />} />
           <Route path="/:id" element={<ProductDetail />} />
         </Routes>
